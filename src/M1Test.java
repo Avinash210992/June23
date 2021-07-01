e43e39287a99a18fc59f5b7c8b69e3ce9c5e4a38
Avinash210992 <avinashsaran92@gmail.com>
2021-07-01 16:59:16 +0530

    first change by sub user on july 01

--- src/M1Test.java
+++ src/M1Test.java
@@ -13,5 +13,14 @@ public class M1Test {
 		System.out.println("this is slave1");
 		System.out.println("this is slave1 change1");
 		System.out.println("this is slave1 change2");
+		
+	
+	}
+	
+	
+	@Test
+	public void m2()
+	{
+		System.out.println("first change made by sub user on jul 01");
 	}
 }
